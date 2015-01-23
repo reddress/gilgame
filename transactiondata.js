3a4be031eb1dbb8bd88e5f8428c455ce161e4be8
Heitor Chang <heitorpontual@gmail.com>
2015-01-23 21:52:18 -0200

    clear transactions

--- transactiondata.js
+++ transactiondata.js
@@ -2,11 +2,5 @@
 // "day-month-year; description; amount; debit; credit",
 
 var transactions = [
-  "30/1/15; expected payment; 300; expenses; assets",
-  "1/1/15; opening balance; 10000,00; assets; open",
-
-  // made-up data
-  "15/1/15; french fries; 100; expenses; assets",
-  "21/1/15; credit card overpaid; 300.00; liabilities; assets",
-  "22/1/15; salario; 500; assets; income",
+  "23/1/15; first gilgame transaction at home;0; equity; open",
 ];
